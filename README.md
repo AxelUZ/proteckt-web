92b5182b282150ca9b175a1a1aa691d99c8d15d0
Axel Uzeta <zaruzeta@outlook.com>
2026-07-11 15:39:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# proteckt-web
+
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-center mt-20">
+      Proteckt Website
+    </h1>
+  );
+}
+
+export default App;
